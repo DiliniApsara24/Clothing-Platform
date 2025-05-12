ae87e70579077d527850be0108b2e15c57d145c6
DiliniApsara24 <diliniapsara605@gmail.com>
2025-05-12 10:15:01 +0530

    signup  page structre completed

---
+++ tutorial/src/Pages/Signup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../common/signup.css";
+
+
+export default function Signup() {
+  return (
+    <div>
+      <div className="main-outer">
+        </div>
+    </div>
+  );
+}
