0d66aa4044861b58b52deff40241a9a0a956f999
DiliniApsara24 <diliniapsara605@gmail.com>
2025-05-16 21:31:01 +0530

    done

--- tutorial/src/Pages/Home.tsx
+++ tutorial/src/Pages/Home.tsx
@@ -13,34 +13,34 @@ const images = [hero1, hero2, hero3, hero4, hero5, hero6];
 
 const slideContent = [
   {
-    title: "Sun-Kissed Styles",
+    title: "Welcome To Cheerpeak Fashion Store",
     description:
-      "Step into summer with confidence and grace. Our latest collection features lightweight linens, breathable cottons, and flowing silhouettes perfect for golden-hour strolls, beachside brunches, and everything in between. Embrace sunshine-ready style with earthy tones and flattering cuts made to turn heads."
+      "Step into summer with confidence and grace. Our latest collection features lightweight linens, breathable cottons."
   },
   {
     title: "Luxe Comfort, Everyday",
     description:
-      "Indulge in everyday luxury with our thoughtfully designed pieces that balance comfort and class. Discover plush knitwear, buttery-soft loungewear, and tailored sets that make casual dressing effortlessly elegant. Because looking good should feel even better."
+      "Indulge in everyday luxury with our thoughtfully designed pieces that balance comfort and class."
   },
   {
     title: "Own the Spotlight",
     description:
-      "Bold colors. Statement silhouettes. Radiant textures. Our power pieces are crafted for those who love to stand out. Whether it’s a dramatic sleeve, a daring cut, or an unforgettable print — these are the looks that let your style do all the talking."
+      "Bold colors. Statement silhouettes. Radiant textures. Our power pieces are crafted for those who love to stand out."
   },
   {
     title: "Classic Never Goes Out of Style",
     description:
-      "Invest in timeless elegance. Explore our curation of classic blazers, crisp white shirts, structured dresses, and smart trousers — all designed with a modern edge. These are the versatile essentials that take you from morning meetings to evening soirées with ease."
+      "Invest in timeless elegance. Explore our curation of classic blazers."
   },
   {
     title: "Chic Redefined",
     description:
-      "Minimal meets modern in this refined collection. Think monochrome palettes, high-waist cuts, oversized blazers, and sculptural accessories. For the fashion lover who believes less is more — and quality speaks louder than trends."
+      "Minimal meets modern in this refined collection. Think monochrome palettes, high-waist cuts, oversized blazers, and sculptural accessories. "
   },
   {
     title: "Effortless Expression",
     description:
-      "Celebrate your unique style with our range of expressive pieces. From artsy prints and vibrant layers to boho chic and streetwear cool, this collection is about freedom — to explore, to express, to wear what makes you feel alive."
+      "Celebrate your unique style with our range of expressive pieces. "
   }
 ];
 
