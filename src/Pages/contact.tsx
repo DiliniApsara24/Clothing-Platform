fed264a3f611f84c938c6678eb1cd55f7de03eae
DiliniApsara24 <diliniapsara605@gmail.com>
2025-06-10 17:49:28 +0530

    contact us page completed

--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -9,14 +9,31 @@ import about2 from "../Images/about-2.jpg";
 import Con1 from "../Images/phone-call.png";
 import Con2 from "../Images/chat.png";
 import Con3 from "../Images/order-tracking.png";
+import contact from "../Images/cont.png"
 
 export default function Contact() {
   return (
     <div>
       <div className="cont-main">
         <div className="contact-outer1">
-          <div className="con-text"></div>
-          <div className="con-pic"></div>
+          <div className="con-text">
+            
+
+          
+          
+          <p>
+              <strong className="c-t">Contact Us About Cheerpeak Fashions </strong><br />
+              <span className="c-m-t">We’d love to help you with anything,whether it’s about your order, our latest styles, sizing help, or anything fashion-related. </span>
+            </p>
+          
+          
+          
+          
+          
+          </div>
+          <div className="con-pic">
+             <img src={contact} className="con-pic" alt="contact" />
+          </div>
         </div>
         <div className="contact-outer2">
           <div className="info-outer">
@@ -31,11 +48,11 @@ export default function Contact() {
                 Phone: +1 (123) 456-0890 <br />
                 Email: support@cheerpeak.com
               </div>
-                  <div className="c-btn">
-                    <button className="con-btn" type="submit">
-                    Send an Email
-                  </button>
-                  </div>
+              <div className="c-btn">
+                <button className="con-btn" type="submit">
+                  Send an Email
+                </button>
+              </div>
             </div>
             <div className="info-1">
               <div className="c-pic">
@@ -47,11 +64,11 @@ export default function Contact() {
                 Not sure about sizing or styling? Chat with our team for quick
                 advice and personalized help.
               </div>
-                  <div className="c-btn1">
-                    <button className="con-btn" type="submit">
-                    Chat Now
-                  </button>
-                  </div>
+              <div className="c-btn1">
+                <button className="con-btn" type="submit">
+                  Chat Now
+                </button>
+              </div>
             </div>
             <div className="info-1">
               <div className="c-pic">
@@ -62,14 +79,12 @@ export default function Contact() {
                 <br />
                 Need help with shipping, returns, or your order status? We're
                 here to help.
-
-                
               </div>
               <div className="c-btn1">
-                    <button className="con-btn" type="submit">
-                    Request Support
-                  </button>
-                </div>
+                <button className="con-btn" type="submit">
+                  Request Support
+                </button>
+              </div>
             </div>
           </div>
         </div>
