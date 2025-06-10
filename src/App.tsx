7816cee226bcfba6e393817c62b43d28afaa8614
DiliniApsara24 <diliniapsara605@gmail.com>
2025-06-10 10:25:36 +0530

    about page structre created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./Pages/Login";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Footer from "./Components/footer";
+import About from "./Pages/about"
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,8 @@ const App: React.FC = () => {
       <Route path="/" element={<Home />} />
        <Route path="/Home" element={<Home />} />
       <Route path="/login" element={<Login />} />
+   <Route path="/about" element={<About />} />
+
     </Routes>
  <Footer />
     </>
---
+++ src/Pages/about.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../common/about.css";
+
+import TextField from "@mui/material/TextField";
+
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <div>
+      <div className="about-main-outer"></div>
+    
+    </div>
+  );
+}
