eb15418fe2d770f77c30c64913662d75d5617ce8
DiliniApsara24 <diliniapsara605@gmail.com>
2025-06-10 15:36:12 +0530

    contact information part complted

--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -4,13 +4,76 @@ import "../common/contact.css";
 import TextField from "@mui/material/TextField";
 
 import { Link } from "react-router-dom";
-import about1 from "../Images/about-1.jpg"
-import about2 from "../Images/about-2.jpg"
+import about1 from "../Images/about-1.jpg";
+import about2 from "../Images/about-2.jpg";
+import Con1 from "../Images/phone-call.png";
+import Con2 from "../Images/chat.png";
+import Con3 from "../Images/order-tracking.png";
 
 export default function Contact() {
   return (
     <div>
-   <div className="cont-main"></div>
+      <div className="cont-main">
+        <div className="contact-outer1">
+          <div className="con-text"></div>
+          <div className="con-pic"></div>
+        </div>
+        <div className="contact-outer2">
+          <div className="info-outer">
+            <div className="info-1">
+              <div className="c-pic">
+                <img src={Con1} className="con1" alt="contact" />
+              </div>
+              <div className="info-t">
+                <strong className="in-tt"> Call or Email Us</strong>
+                <br />
+                Want to speak to someone directly? <br />
+                Phone: +1 (123) 456-0890 <br />
+                Email: support@cheerpeak.com
+              </div>
+                  <div className="c-btn">
+                    <button className="con-btn" type="submit">
+                    Send an Email
+                  </button>
+                  </div>
+            </div>
+            <div className="info-1">
+              <div className="c-pic">
+                <img src={Con2} className="con1" alt="contact" />
+              </div>
+              <div className="info-t">
+                <strong className="in-tt">Chat With Our Style Team</strong>
+                <br />
+                Not sure about sizing or styling? Chat with our team for quick
+                advice and personalized help.
+              </div>
+                  <div className="c-btn1">
+                    <button className="con-btn" type="submit">
+                    Chat Now
+                  </button>
+                  </div>
+            </div>
+            <div className="info-1">
+              <div className="c-pic">
+                <img src={Con3} className="con1" alt="contact" />
+              </div>
+              <div className="info-t1">
+                <strong className="in-tt">Track or Ask About an Order</strong>
+                <br />
+                Need help with shipping, returns, or your order status? We're
+                here to help.
+
+                
+              </div>
+              <div className="c-btn1">
+                    <button className="con-btn" type="submit">
+                    Request Support
+                  </button>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
