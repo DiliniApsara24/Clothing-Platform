699d8e91e8d19c67ad248baae55b2330d55ce275
DiliniApsara24 <diliniapsara605@gmail.com>
2025-06-12 07:18:20 +0530

    page completed

--- src/Pages/shopMen.tsx
+++ src/Pages/shopMen.tsx
@@ -5,10 +5,10 @@ import TextField from "@mui/material/TextField";
 
 import { Link } from "react-router-dom";
 import menpic from "../Images/men-pic.png";
-import cat1 from "../Images/cate1.png"
-import cat2 from "../Images/cate2.png"
-import cat3 from "../Images/cate3.png"
-import cat4 from "../Images/cate4.png"
+import cat1 from "../Images/cate1.png";
+import cat2 from "../Images/cate2.png";
+import cat3 from "../Images/cate3.png";
+import cat4 from "../Images/cate4.png";
 
 export default function shopMen() {
   return (
@@ -35,29 +35,68 @@ export default function shopMen() {
           </div>
         </div>
         <div className="men-o-3">
-          <div className="catg-m-01">
-            <div className="men-c-p">
-            <img src={cat1} className="catg-pic" alt="cate" />
-</div>
-            <div className="shop-btn"></div>
+          <div className="men-c-p">
+            <div className="item-t">
+              <div className="item-n">T-Shirts & Polos</div>
+            </div>
+            <div className="catg-m-01">
+              <img src={cat1} className="catg-pic" alt="cate" />
+              <div className="item-t">
+                <div className="shop-btn">
+                     <button className="men-btn" type="submit">
+                  Shop Now
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="catg-m-01">
-            <div className="men-c-p">
-            <img src={cat2} className="catg-pic" alt="cate" />
-</div>
-            <div className="shop-btn"></div>
+
+          <div className="men-c-p">
+            <div className="item-t">
+              <div className="item-n">Shirts</div>
+            </div>
+            <div className="catg-m-01">
+              <img src={cat2} className="catg-pic" alt="cate" />
+              <div className="item-t">
+                <div className="shop-btn">
+                     <button className="men-btn" type="submit">
+                  Shop Now
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="catg-m-01">
-             <div className="men-c-p">
-            <img src={cat3} className="catg-pic" alt="cate" />
-</div>
-            <div className="shop-btn"></div>
+
+          <div className="men-c-p">
+            <div className="item-t">
+              <div className="item-n">Hoodies & Sweatshirts</div>
+            </div>
+            <div className="catg-m-01">
+              <img src={cat3} className="catg-pic" alt="cate" />
+              <div className="item-t">
+                <div className="shop-btn">
+                     <button className="men-btn" type="submit">
+                  Shop Now
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="catg-m-01">
-             <div className="men-c-p">
-            <img src={cat4} className="catg-pic" alt="cate" />
-</div>
-            <div className="shop-btn"></div>
+
+          <div className="men-c-p">
+            <div className="item-t">
+              <div className="item-n">Pants & Trousers</div>
+            </div>
+            <div className="catg-m-01">
+              <img src={cat4} className="catg-pic" alt="cate" />
+              <div className="item-t">
+                <div className="shop-btn">
+                  <button className="men-btn" type="submit">
+                  Shop Now
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
