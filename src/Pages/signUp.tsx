357dda5378378cd417af25e80b44005e459725bb
DiliniApsara24 <diliniapsara605@gmail.com>
2025-06-17 17:29:52 +0530

    text 1

--- src/Pages/signUp.tsx
+++ src/Pages/signUp.tsx
@@ -7,6 +7,7 @@ import icon1 from "../Images/icon1.png";
 import icon2 from "../Images/icon2.png";
 import icon3 from "../Images/icon3.png";
 import { Link } from "react-router-dom";
+import 
 
 export default function Signup() {
   return (
@@ -15,7 +16,7 @@ export default function Signup() {
         <div className="main-sub-sign">
           <div className="first-outer-sign"></div>
           <div className="second-outer-sign">
-            <img src="" alt="" />
+            <img src={signup}  className="sign-up"alt="signup" />
           </div>
         </div>
       </div>
