4be5dfd66fba2c1f73fd2ceef507ae5278e7a569
DiliniApsara24 <diliniapsara605@gmail.com>
2025-06-20 11:36:42 +0530

    structre done for sign-up

--- src/Pages/signUp.tsx
+++ src/Pages/signUp.tsx
@@ -7,7 +7,7 @@ import icon1 from "../Images/icon1.png";
 import icon2 from "../Images/icon2.png";
 import icon3 from "../Images/icon3.png";
 import { Link } from "react-router-dom";
-import logosign from "../Images/logo.png"
+import logosign from "../Images/logo.png";
 
 export default function Signup() {
   return (
@@ -15,26 +15,44 @@ export default function Signup() {
       <div className="sign-main-outer">
         <div className="main-sub-sign">
           <div className="first-outer-sign">
-             <img src={background} className="background-img-sign" alt="background" />
+            <img
+              src={background}
+              className="background-img-sign"
+              alt="background"
+            />
           </div>
           <div className="second-outer-sign">
             <div className="sec-sign">
               <div className="sign-logo-p">
-                             <div className="sign-logo">
-
-                 <img src={logosign} className="logo-sign-1" alt="logo" />
+                <div className="sign-logo">
+                  <img src={logosign} className="logo-sign-1" alt="logo" />
+                </div>
               </div>
-              </div> 
               <div className="sign-text-P">
-              <div className="sign-text-1"></div>
-              <div className="sign-text-1"></div>
+                <div className="sign-text-1">
+                  <div className="sign-sub"></div>
+                  <div className="sign-sub"></div>
+                  <div className="sign-sub"></div>
+                </div>
+                <div className="sign-text-1">
+                  <div className="sign-sub"></div>
+                  <div className="sign-sub"></div>
+                  <div className="sign-sub"></div>
+                </div>
               </div>
+               <div className="sign-text-button">
+
+                <div className="button-outer-sign">
+                  <button className="sign-button" type="submit">
+                    Sign Up
+                  </button>
+                </div>
+               </div>
             </div>
+           
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-
