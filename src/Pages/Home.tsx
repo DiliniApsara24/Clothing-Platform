6a5cd2b257167c3552e34b86c1e2f212122dd514
DiliniApsara24 <diliniapsara605@gmail.com>
2025-08-03 15:34:52 +0530

    done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,7 +7,7 @@ import hero2 from "../Images/dress2.jpg";
 import hero3 from "../Images/dress3.jpg";
 import hero4 from "../Images/dress4.jpg";
 import hero5 from "../Images/dress5.jpg";
-import hero6 from "../Images/dress6.png";
+import hero6 from "../Images/dress6.jpg";
 import hero7 from "../Images/dress7.jpg";
 
 
